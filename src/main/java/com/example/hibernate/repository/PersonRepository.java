a959411b7a51f41617f62ad3a88ebfa484427031
Sokolov Pavel <sokolovpavel31@gmail.com>
2026-05-09 16:11:41 +0700

    fix: query now specifies the full path to the table

--- src/main/java/com/example/hibernate/repository/PersonRepository.java
+++ src/main/java/com/example/hibernate/repository/PersonRepository.java
@@ -11,12 +11,12 @@ import java.util.Optional;
 
 @Repository
 public interface PersonRepository extends JpaRepository<Person, Long> {
-    @Query(value = "SELECT * FROM persons WHERE city = :city", nativeQuery = true)
+    @Query(value = "SELECT * FROM demojoin.persons WHERE city = :city", nativeQuery = true)
     List<Person> findByCity(@Param("city") String city);
 
-    @Query(value = "SELECT * FROM persons WHERE age < :age ORDER BY age", nativeQuery = true)
+    @Query(value = "SELECT * FROM demojoin.persons WHERE age < :age ORDER BY age", nativeQuery = true)
     List<Person> findByAgeLessThanOrderByAgeAsc(@Param("age") int age);
 
-    @Query(value = "SELECT * FROM persons WHERE name = :name AND surname = :surname", nativeQuery = true)
+    @Query(value = "SELECT * FROM demojoin.persons WHERE name = :name AND surname = :surname", nativeQuery = true)
     Optional<Person> findByNameAndSurname(@Param("name") String name, @Param("surname") String surname);
 }
